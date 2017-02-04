7e8b7dde98ee2adcc30d3cb3d80fe0cdec0564ad
Abhishek Badjatya <abhishekbadjatya@dyn-160-39-228-91.dyn.columbia.edu>
2017-02-04 17:06:38 -0500

    Updated gateway service to build skeleton for fetching details from user service

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,28 +1,47 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 var config=require('../config.js');
 var stripe = require('stripe')(config.STRIPE_SECRET_KEY);
+var fetch = require('node-fetch');
 
 var app = express();
-app.use(bodyParser());
+// app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Test api for getting charge details
+app.get('/getCharge', function(req, res) {
+    // res.writeHead(200, {"Content-Type": "application/json"});
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send({"charge":2000});
+});
+
+//Api for chargin based on token
 app.post('/charge', function(req, res) {
     var stripeToken = req.body.stripeToken;
-    console.log(stripeToken);
-    var amount = 1000;
-
-    stripe.charges.create({
+    fetch('http://localhost:3000/getCharge')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        var amount = json.charge;
+        stripe.charges.create({
         card: stripeToken,
         currency: 'usd',
         amount: amount
-    },
-    function(err, charge) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            console.log(charge);
-            res.status(200).send(charge);
-        }
+     },function(err, charge) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                console.log(charge);
+                res.status(200).send(charge);
+            }
+        })
+    })
+    .catch(function(err) {
+        console.log('Error');
+        res.status(500).send(err);
     });
 });
 

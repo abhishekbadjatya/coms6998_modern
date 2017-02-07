dbc760a4af927329ab6379ae79ef88892436497d
Abhishek Badjatya <abhishekbadjatya@dyn-160-39-228-239.dyn.columbia.edu>
2017-02-07 16:37:34 -0500

    Added accesing headers for JWT

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -4,7 +4,10 @@ var http = require('http');
 var config=require('../config.js');
 var stripe = require('stripe')(config.STRIPE_SECRET_KEY);
 var fetch = require('node-fetch');
+// var expressJWT = require('express-jwt');
+// var jwt = require ('jsonwebtoken');
 var myHeaders= {'Content-Type': 'application/json'};
+// const SECRET_KEY = "This is the secret key";
 
 var app = express();
 // app.use(bodyParser());
@@ -20,6 +23,8 @@ app.get('/api/user/grocery/123456789', function(req, res) {
 
 //
 app.post('/api/gateway/charge', function(req, res) {
+    console.log(req.headers);
+    var JWT=req.headers.authorization;
     var stripeToken = req.body.stripeToken; //need to check from where to retrieve the token
     productid=123456789
     fetch('http://localhost:3000/api/user/grocery/'+productid)
@@ -39,12 +44,13 @@ app.post('/api/gateway/charge', function(req, res) {
             } else {
                 var postdata = {
                     "chargeDetails":charge,
-                    "JWT":"JWTToken",
                     "product_id":productid
                 };
+                var newHeader={'Content-Type': 'application/json',
+                            'Authorization': JWT};
                 fetch('http://localhost:3001/api/payment/save', { method: 'POST', 
                                                                 body: JSON.stringify(postdata),
-                                                                headers: myHeaders
+                                                                headers: newHeader
                 })
                 .then(function(res){
                     console.log('In then');
@@ -63,4 +69,4 @@ app.post('/api/gateway/charge', function(req, res) {
 });
 
 app.use(express.static(__dirname));
-app.listen(3000);
+app.listen(3002);

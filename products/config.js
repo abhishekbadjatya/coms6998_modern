cdb96be1e221b18403e724bd98dfe352cc509744
Tushar Agarwal <tusharag171@gmail.com>
2017-03-29 20:22:44 -0400

    Products Microservice

---
+++ products/config.js
@@ -0,0 +1,6 @@
+var config = {}
+
+config.MONGO_DB_CONNECTION_STRING="mongodb://apps:apps@ds143000.mlab.com:43000/product_db" 
+config.BASE_URL_USER="http://localhost:3005/"
+	
+module.exports = config;

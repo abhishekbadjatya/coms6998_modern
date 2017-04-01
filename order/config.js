74e4b0ac1126ca3f4ae0dc79b0f8c28ff874f022
Anand Naik <anand.naik@columbia.edu>
2017-03-31 23:07:24 -0400

    Config js added

--- order/config.js
+++ order/config.js
@@ -1,8 +1,14 @@
 var config = {}
 
+
 config.MONGO_DB_CONNECTION_STRING=""
-config.BASE_URL_USER="http://localhost:3000"
+config.BASE_URL_USER=""
+config.takeProductARN = ""
 
-
+config.AWS_config = {
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: ''
+}
 
 module.exports = config;

bda0cad2d753a36128758db6e3ff175d5927b06b
Tushar <ta2482@columbia.edu>
2017-04-25 01:16:45 -0400

    swagger

--- order/config.js
+++ order/config.js
@@ -1,14 +1,14 @@
 var config = {}
 
 
-config.MONGO_DB_CONNECTION_STRING=""
+config.MONGO_DB_CONNECTION_STRING="mongodb://aastv_order:aastv@ds141950.mlab.com:41950/order_ms"
 config.BASE_URL_USER=""
 config.takeProductARN = ""
 
 config.AWS_config = {
-  accessKeyId: '',
-  secretAccessKey: '',
-  region: ''
+  accessKeyId: 'AKIAII2PB7O2Q7W4A77A',
+  secretAccessKey: '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',
+  region: 'us-west-2'
 }
 
 module.exports = config;

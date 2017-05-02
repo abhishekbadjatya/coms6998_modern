4acc0365c3a19530cafc223bfd105f1140398c96
Anand Naik <anand.naik@columbia.edu>
2017-05-02 18:00:54 -0400

    config and package.json in sqs worker and poller

---
+++ sqs_worker/config.js
@@ -0,0 +1,13 @@
+var config = {}
+config.BASE_URL=""
+config.mySqsURL = ""
+
+config.AWS_config = {
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: ''
+}
+
+config.worker = "";
+
+module.exports = config;
